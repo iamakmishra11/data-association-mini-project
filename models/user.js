fb650e1c687e8aedc73db044d45ec682587487c8
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-09-17 18:34:49 +0530

    made changes to fix errors
    made changes

--- models/user.js
+++ models/user.js
@@ -6,10 +6,7 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
@@ -17,7 +14,6 @@ const connectToMongoDB = async () => {
 };
 
 connectToMongoDB();
-
 
 const userSchema = new mongoose.Schema({
   username: String,
