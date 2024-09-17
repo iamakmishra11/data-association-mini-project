c56db1bb15a6d50d5eb83aca231928fd9992cce0
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-09-17 18:02:47 +0530

    made some minor mongoose changes
    mongoose connection

--- models/user.js
+++ models/user.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/miniproject");
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/miniproject", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
+
+connectToMongoDB();
 
 const userSchema = new mongoose.Schema({
   username: String,
