8eeab6fb8c7075e2433cab056369d4fb8e5b1955
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-09-17 18:17:02 +0530

    local host change
    localhost changed

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose');
 
+
+
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/miniproject", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -14,6 +17,7 @@ const connectToMongoDB = async () => {
 };
 
 connectToMongoDB();
+
 
 const userSchema = new mongoose.Schema({
   username: String,
