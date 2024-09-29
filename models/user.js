60af09f6997d4b90bd75d22933f90ce778eaf6d1
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-09-29 08:35:17 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(mongodb+srv://anujmishra26062002:akmishra@cluster0.yrxpx.mongodb.net/);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
